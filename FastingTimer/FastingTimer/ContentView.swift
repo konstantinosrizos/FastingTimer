507b50c2159de7d6f2bf0f11fa78aed288c850a7
konstantinosrizos <rizos.dev@gmail.com>
2022-08-27 16:42:16 +0100

    UI created

--- FastingTimer/FastingTimer/ContentView.swift
+++ FastingTimer/FastingTimer/ContentView.swift
@@ -20,11 +20,67 @@ struct ContentView: View {
     }
 	
 	var content: some View {
-		VStack {
-			// MARK: Progress Ring
-			
-			ProgressRingView()
+		ZStack {
+			VStack(spacing: 40) {
+				// MARK: Title
+				Text("Let's get started!")
+					.font(.headline)
+					.foregroundColor(Color(#colorLiteral(red: 0.2616114616, green: 0.3207069039, blue: 1, alpha: 1)))
+				
+				// MARK: Fasting Plan
+				Text("16:8")
+					.fontWeight(.semibold)
+					.padding(.horizontal, 24)
+					.padding(.vertical, 8)
+					.background(.thinMaterial)
+					.cornerRadius(20)
+				
+				Spacer()
+			}
+			.padding()
+				
+			VStack(spacing: 40) {
+				// MARK: Progress Ring
+				ProgressRingView()
+				
+				HStack(spacing: 60) {
+					// MARK: Start Timer
+					VStack(spacing: 5) {
+						Text("Start")
+							.opacity(0.7)
+						
+						Text(Date(), format: .dateTime.weekday().hour().minute().second())
+							.fontWeight(.bold)
+					}
+					
+					// MARK: End Timer
+					VStack(spacing: 5) {
+						Text("End")
+							.opacity(0.7)
+						
+						Text(Date().addingTimeInterval(16), format: .dateTime.weekday().hour().minute().second())
+							.fontWeight(.bold)
+					}
+				}
+				
+				// MARK: Button
+				Button {
+					
+				} label: {
+					Text("Start fasting")
+						.font(.title3)
+						.fontWeight(.semibold)
+						.padding(.horizontal, 24)
+						.padding(.vertical, 8)
+						.background(.thinMaterial)
+						.cornerRadius(20)
+				}
+
+				
+			}
+			.padding()
 		}
+		.foregroundColor(.white)
 	}
 }
 
