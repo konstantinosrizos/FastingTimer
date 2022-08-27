e4894aead88006ab8a311a0efa64dd0954a74282
konstantinosrizos <rizos.dev@gmail.com>
2022-08-27 15:52:48 +0100

    Initialising the project

---
+++ FastingTimer/FastingTimer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FastingTimer
+//
+//  Created by Konstantinos Rizos on 27/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FastingTimer/FastingTimer/FastingTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastingTimerApp.swift
+//  FastingTimer
+//
+//  Created by Konstantinos Rizos on 27/8/22.
+//
+
+import SwiftUI
+
+@main
+struct FastingTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
